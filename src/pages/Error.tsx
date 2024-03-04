be10870cb37671ab267374c55ad2d7152707be15
Flavio Ever <flavio.ever@outlook.com>
2024-03-04 20:02:47 -0300

    chore: cleaning page error

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,19 +1,10 @@
-import { useRouteError } from 'react-router-dom';
-
 const ErrorPage: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const error: any = useRouteError();
-  console.error(error);
-
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
         <div className="flex flex-col gap-2">
           <h1 className="text-3xl">Oops!</h1>
-          <p>Desculpe, algum erro inesperado ocorreu.</p>
-          <p>
-            Erro: <i>{error?.statusText || error?.message}</i>
-          </p>
+          <p>Desculpe, pagina não encontrada.</p>
         </div>
       </div>
     </>
